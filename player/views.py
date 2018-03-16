913fe33e765a889f67cceb892574f1c6ce859917
Faheel Ahmad <faheel@live.in>
2018-03-16 20:20:54 +0530

    Add config view

--- player/views.py
+++ player/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'player/index.html', {})
+    return render(request, 'player/base.html', {})
+
+
+def config(request):
+    return render(request, 'player/config.html', {})
