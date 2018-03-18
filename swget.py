12e20a6a2d7f5e26a5c3fd3d02c3067d5abb5b2e
huzaifafaruqui <huzaifafaruqui@gmail.com>
2018-03-18 18:54:29 +0530

    bug fix

--- swget.py
+++ swget.py
@@ -2,8 +2,10 @@ import wget
 from tqdm import tqdm
 
 YEAR = '2018'
-MONTH = 'JAN'
+MONTH = 'FEB'
 
+path = '/home/huzaifa/sih/data/feb/'
+            
 
 for date in tqdm(range(1, 31)):
     hour = 0
@@ -13,18 +15,20 @@ for date in tqdm(range(1, 31)):
         
         url = 'https://www.mosdac.gov.in/data/servlet/Image?image=preview&loc=/web/mosdac_preview/3D_IMG/preview/{0}/{1:02d}{2}/3DIMG_{1:02d}{2}{0}_{3:02d}{4:02d}_L1C_ASIA_MER_IR1.jpg'.format(YEAR, date, MONTH, hour, minute)
         #print(url)
+        output = '3DIMG_{1:02d}{2}{0}_{3:02d}{4:02d}_L1C_ASIA_MER_IR1.jpg'.format(YEAR, date, MONTH, hour, minute)
+        try:
+            filename = wget.download(url, out=path+output)
+        except:
+            pass
+        
+        
+        
         minute += 30
         if minute >= 60:
             hour += 1
             minute %= 60
         
-        if hour >= 24:
+        if hour > 23:
             break
         
         #time.sleep(1)
-        try:
-            path = '/home/huzaifa/sih/data/sih/'
-            output = '3DIMG_{1:02d}{2}{0}_{3:02d}{4:02d}_L1C_ASIA_MER_IR1.jpg'.format(YEAR, date, MONTH, hour, minute)
-            filename = wget.download(url, out=path+output)
-        except:
-            pass
