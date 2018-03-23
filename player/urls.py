97041236c17c5d44000d7b61d3066695dd0b0308
Faheel Ahmad <faheel@live.in>
2018-03-23 23:21:20 +0530

    Add login and logout functionality
    and add blank upload page layout

--- player/urls.py
+++ player/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('play', views.play, name='play'),
     path('config', views.config, name='config'),
+    path('login/', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
+    path('play', views.play, name='play'),
+    path('upload', views.upload, name='upload'),
 ]
--- player/views.py
+++ player/views.py
@@ -1,8 +1,39 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
+from .forms import LoginForm
 
 
 def index(request):
     return render(request, 'player/base.html', {})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(request.GET.get('next', '/'))
+        else:
+            is_invalid = True
+    else:
+        login_form = LoginForm()
+        is_invalid = False
+    
+    return render(request, 'player/login.html', {
+        'form': login_form,
+        'is_invalid': is_invalid,
+    })
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
 
 
 def config(request):
@@ -11,3 +42,8 @@ def config(request):
 
 def play(request):
     return render(request, 'player/play.html', {})
+
+
+@login_required
+def upload(request):
+    return render(request, 'player/upload.html', {})
