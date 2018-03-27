7dd68859260c3bf4cfb9de795c4b3e0c12e8056c
Faheel Ahmad <faheel@live.in>
2018-03-27 14:40:21 +0530

    Add Image model

--- player/models.py
+++ player/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='uploads')
+    
+    CLOUD_COVER = 'cloud_cover'
+    IMAGE_TYPE_CHOICES = (
+        (CLOUD_COVER, 'Cloud cover'),
+    )
+    image_type = models.CharField(max_length=64, choices=IMAGE_TYPE_CHOICES, default=CLOUD_COVER)
+    
+    date = models.DateField()
+    time = models.TimeField()
+    
+    class Meta:
+        ordering = ['date', 'time']
