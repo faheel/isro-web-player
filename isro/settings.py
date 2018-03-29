1e7e1b5610c57335755f23072cf4854632c31e92
Faheel Ahmad <faheel@live.in>
2018-03-29 21:57:31 +0530

    Add media root and URL
    to be able to load uploaded images

--- isro/settings.py
+++ isro/settings.py
@@ -137,3 +137,6 @@ STATIC_URL = '/static/'
 # Login URL
 
 LOGIN_URL = '/login/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- isro/urls.py
+++ isro/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('', include('player.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
