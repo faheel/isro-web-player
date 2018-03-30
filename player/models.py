5780031c6002df603b7ada039b457bfa3578a926
Gaurav Arora <gaurav.arora0077@gmail.com>
2018-03-30 16:58:11 +0530

    Fix date validation and encrypt image names (#1)

--- player/models.py
+++ player/models.py
@@ -2,14 +2,19 @@ from django.db import models
 
 
 class Image(models.Model):
-    image = models.ImageField(upload_to='uploads')
+    def get_file_path(instance, filename):
+        self.name=filename
+        ext = filename.split('.')[-1]
+        filename = "%s.%s" % (uuid.uuid4(), ext)
+        return os.path.join('uploads', filename)
+    image = models.ImageField(upload_to=get_file_path)
     
     CLOUD_COVER = 'cloud_cover'
     IMAGE_TYPE_CHOICES = (
         (CLOUD_COVER, 'Cloud cover'),
     )
     image_type = models.CharField(max_length=64, choices=IMAGE_TYPE_CHOICES, default=CLOUD_COVER)
-    
+    name=models.CharField(max_length=64)
     date_time = models.DateTimeField()
     
     class Meta:
