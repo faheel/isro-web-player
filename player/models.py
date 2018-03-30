1c4655d474ba2be3c5d92949be4dd3be4a23346e
gaurav61 <gaurav.arora0077@gmail.com>
2018-03-30 14:51:54 +0530

    image encryption

--- player/models.py
+++ player/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Image(models.Model):
     def get_file_path(instance, filename):
+        self.name=filename
         ext = filename.split('.')[-1]
         filename = "%s.%s" % (uuid.uuid4(), ext)
         return os.path.join('uploads', filename)
@@ -13,7 +14,7 @@ class Image(models.Model):
         (CLOUD_COVER, 'Cloud cover'),
     )
     image_type = models.CharField(max_length=64, choices=IMAGE_TYPE_CHOICES, default=CLOUD_COVER)
-    
+    name=models.CharField(max_length=64)
     date_time = models.DateTimeField()
     
     class Meta:
